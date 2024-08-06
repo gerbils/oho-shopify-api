376b29fc5ce65ef2b077b01950a2734c18e5da68
pragdave <pragdave@gmail.com>
2024-08-06 10:20:18 -0500

    revert back from individual cursors

--- lib/oho_shopify_api/order.rb
+++ lib/oho_shopify_api/order.rb
@@ -22,8 +22,26 @@ module OhoShopifyApi::Order
   end
 
   # Download all orders since the given datetime, yielding each in turn
-  # to the given block. Return the final cursor so we can continue where
-  # we left off next time
+  # to the given block.
+  #
+  # This is gnarlier than it should be.
+  #
+  # Shopify updatedBy is rounded to the second, but internsally it must have more accurary.
+  #
+  # When we fetch new orders, we ask for those with sn updatedAt > that of the last order we
+  # processed previously. However, that typically results it at least one duplicate being returned
+  # at the start of the response: the the order was updated at 12:34:56.3, we ask for orders
+  # updated after 12:34:56, and hence get it again.
+  #
+  # So, when we fetch orders, we pass them one at a time to the callback code, and it determines
+  # if that order is a duplicate. If so, we don't decrement the count of orders processed
+  #
+  # For efficiency we download from shopify in batches using a cursor. Normally you'd
+  # stop making requests when the quest returns hasNextPage=false. We can't do that
+  # because if we initially asked for 10 orders, then the response would say "here's all 10, no
+  # next page." But if we skipped 2 orders we have to ignore that and go fetch another 2 using
+  # the cursor of the last order previously fetched
+
   def from_filter(max_to_fetch, filter, &callback)
     puts "top of from_filter: #{max_to_fetch} #{filter}"
     pageInfo = {}
@@ -33,11 +51,14 @@ module OhoShopifyApi::Order
 
     begin
       count = [left_to_fetch, ORDERS_PER_FETCH].min
+      need_additional = false
+
       variables = {
         filter: filter,
         limit:   count,
         lastCursor: cursor
       }
+
       puts("Fetching orders with parameters: #{variables.inspect}")
       raw_result = OhoShopifyApi::Client.query(OhoShopifyApi::Queries::Order::Fetch, variables:)
       result   = raw_result.to_hash
@@ -56,11 +77,12 @@ module OhoShopifyApi::Order
         when :ok
           left_to_fetch -= 1
         when :restart_after_current
+          need_additional = true
           puts "skipping"
         end
       end
-      puts "end of loop, #{left_to_fetch} #{cursor}"
-    end while !error_seen && left_to_fetch > 0 && pageInfo["hasNextPage"]
+      puts "end of loop, #{need_additional}: #{left_to_fetch} #{cursor}"
+    end while !error_seen && left_to_fetch > 0 && (need_additional || pageInfo["hasNextPage"])
   end
 
 end
